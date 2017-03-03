a2bafaff3674adede76837abaa8ac8cea67ca7c4
Nicolas Orlandini <nicolas.orlandini@outlook.fr>
2017-03-03 17:25:19 +0100

    Création de la page Réalisations
    - Création des cards de la page Réalisation décrivant les projets réalisés
    - Modification des couleurs du menu et du footer

--- serviceWorker.js
+++ serviceWorker.js
@@ -2,7 +2,7 @@
  * Attention à la version de l'application
  */
 // nom du cache
-var cacheName = 'cerm v0.7';
+var cacheName = 'cerm v0.8';
 // fichiers à mettre en cache
 var filesToCache = [
     '/',
