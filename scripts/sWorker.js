9aa85bdd864ac214ad4fdc3ee29daaf6876c032b
Singu_Admin <valentin.leon0@gmail.com>
2017-03-04 16:19:35 +0100

    Modification de la page des formations
    Ajout d'images
    Remplacement du lien snapchat par google+
    Remplacement du lien google+ par linkedin
    Modification de la timeline

--- scripts/sWorker.js
+++ scripts/sWorker.js
@@ -4,14 +4,3 @@ if ('serviceWorker' in navigator) {
         .register('./serviceWorker.js')
         .then(function() { console.log('Service Worker Registered'); });
 }
-// if ('serviceWorker' in navigator) {
-//     window.addEventListener('load', function() {
-//         navigator.serviceWorker.register('./serviceWorker.js').then(function(registration) {
-//             // Registration was successful
-//             console.log('ServiceWorker registration successful with scope: ', registration.scope);
-//         }).catch(function(err) {
-//             // registration failed :(
-//             console.log('ServiceWorker registration failed: ', err);
-//         });
-//     });
-// }
--- serviceWorker.js
+++ serviceWorker.js
@@ -2,7 +2,7 @@
  * Attention à la version de l'application
  */
 // nom du cache
-var cacheName = 'cerm v0.15';
+var cacheName = 'cerm v0.16';
 // fichiers à mettre en cache
 var filesToCache = [
     '/',
