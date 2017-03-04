121531232cd47f8b475e9cf4aad657ee6b7766c9
Singu_Admin <valentin.leon0@gmail.com>
2017-03-04 03:00:46 +0100

    Création de la page A propos de moi
    Ajout d'image pour l'avatar
    Ajout d'une image pour le fond du navigation drawer
    Ajout d'une fonction js pour l'envoi de mail (A modifier avec MailJS si possible)

--- serviceWorker.js
+++ serviceWorker.js
@@ -2,7 +2,7 @@
  * Attention à la version de l'application
  */
 // nom du cache
-var cacheName = 'cerm v0.8';
+var cacheName = 'cerm v0.9';
 // fichiers à mettre en cache
 var filesToCache = [
     '/',
