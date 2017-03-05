728750b6cd25f83327bde2ab5956d8147ee7b3fc
Nicolas Orlandini <nicolas.orlandini@outlook.fr>
2017-03-05 11:34:21 +0100

    Amélioration de l'user view du side menu (texte + image) sur chaque page

--- serviceWorker.js
+++ serviceWorker.js
@@ -2,7 +2,7 @@
  * Attention à la version de l'application
  */
 // nom du cache
-var cacheName = 'cerm v0.20';
+var cacheName = 'cerm v0.22';
 // fichiers à mettre en cache
 var filesToCache = [
     '/',
