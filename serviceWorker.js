b5248ee51d857cac0e50a723b2e5053ade8e0a15
Nicolas Orlandini <nicolas.orlandini@outlook.fr>
2017-03-05 10:44:51 +0100

    Amélioration du design de la page Compétences

--- serviceWorker.js
+++ serviceWorker.js
@@ -2,7 +2,7 @@
  * Attention à la version de l'application
  */
 // nom du cache
-var cacheName = 'cerm v0.19';
+var cacheName = 'cerm v0.20';
 // fichiers à mettre en cache
 var filesToCache = [
     '/',
