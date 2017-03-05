c94ac33a2a40915bd1b176a9bbed288e52228ff4
Singu_Admin <valentin.leon0@gmail.com>
2017-03-05 23:23:48 +0100

    Test si envoi marche en ligne pour le contact

--- scripts/contact.js
+++ scripts/contact.js
@@ -1,4 +1,3 @@
-console.log("coucuo");
 $('#btnSend').click(function(e) {
     var name = document.getElementById('last_name');
     var subject = document.getElementById('subject');
@@ -9,12 +8,23 @@ $('#btnSend').click(function(e) {
         alertify.error('Attention des champs sont vides');
     } else {
         $.ajax({
-            url: "https://formspree.io/valentin.leon0@gmail.com",
-            method: "POST",
-            data: $('#contactForm').serialize(),
-            dataType: "json"
+            'type': "POST",
+            'url': "https: //mandrillapp.com/api/1.0/messages/send.json",
+            'data': {
+                'key': "WPGvXk3CZAHrZ11V0c0",
+                'message': {
+                    'from_email': mail.value,
+                    'to': [{
+                        'email': 'valentin.leon0@gmail.com',
+                        'type': 'to'
+                    }],
+                    'autotext': true,
+                    'subject': subject.value,
+                    'html': message.value
+                }
+            }
+        }).done(function(response) {
+            console.log(response);
         });
-        e.preventDefault();
-        alertify.success('Message envoyé');
     }
 });
