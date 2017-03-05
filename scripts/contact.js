256aca2b28d9ef1b371a3d2e23902e0080a6c94d
Singu_Admin <valentin.leon0@gmail.com>
2017-03-05 23:31:46 +0100

    Nouveau test online contact

--- scripts/contact.js
+++ scripts/contact.js
@@ -1,30 +1,32 @@
-$('#btnSend').click(function(e) {
-    var name = document.getElementById('last_name');
-    var subject = document.getElementById('subject');
-    var message = document.getElementById('message');
-    var mail = document.getElementById('email');
-
-    if (!subject.value || !message.value) {
-        alertify.error('Attention des champs sont vides');
-    } else {
-        $.ajax({
-            'type': "POST",
-            'url': "https: //mandrillapp.com/api/1.0/messages/send.json",
-            'data': {
-                'key': "WPGvXk3CZAHrZ11V0c0",
-                'message': {
-                    'from_email': mail.value,
-                    'to': [{
-                        'email': 'valentin.leon0@gmail.com',
-                        'type': 'to'
-                    }],
-                    'autotext': true,
-                    'subject': subject.value,
-                    'html': message.value
-                }
+$('#btnSend').click(function() {
+    $.ajax({
+        type: 'POST',
+        url: 'https://mandrillapp.com/api/1.0/messages/send.json',
+        data: {
+            'key': 'WPGvXk3CZAHrZ11V0c0-2A',
+            'message': {
+                'from_email': 'valentin.leon0@gmail.com',
+                'to': [{
+                    'email': $('.email').val(),
+                    'name': $('.name').val(),
+                    'type': 'to'
+                }],
+                // optional merge variables. must also be setup on the list management side of mandrill
+                'merge_vars': [{
+                    'rcpt': $('.email').val(),
+                    'vars': [{
+                        'name': 'COOLFRIEND',
+                        "content": 'Mike'
+                    }, {
+                        'name': 'YEARS',
+                        'content': '27'
+                    }]
+                }],
+                'autotext': 'true',
+                'subject': 'EMAIL_SUBJECT_GOES_HERE',
             }
-        }).done(function(response) {
-            console.log(response);
-        });
-    }
+        }
+    }).done(function(response) {
+        console.log(response); // if you're into that sorta thing
+    });
 });
--- serviceWorker.js
+++ serviceWorker.js
@@ -2,7 +2,7 @@
  * Attention à la version de l'application
  */
 // nom du cache
-var cacheName = 'cerm v1.0';
+var cacheName = 'cerm v1.0.2';
 // fichiers à mettre en cache
 var filesToCache = [
     '/',
