f724de2b902caf5d640b34d75cc760c812034345
Singu_Admin <valentin.leon0@gmail.com>
2017-03-05 17:44:17 +0100

    Suppression du "true" à la fin de la clé api google maps

--- serviceWorker.js
+++ serviceWorker.js
@@ -2,7 +2,7 @@
  * Attention à la version de l'application
  */
 // nom du cache
-var cacheName = 'cerm v0.25';
+var cacheName = 'cerm v0.26';
 // fichiers à mettre en cache
 var filesToCache = [
     '/',
